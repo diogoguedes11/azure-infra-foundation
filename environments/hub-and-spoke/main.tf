4f15ae836b2114bbd4e22ad749355ca66adcd531
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-01-06 16:23:10 +0000

    feat: for testing purposes adding public ip for the vm.

--- environments/hub-and-spoke/main.tf
+++ environments/hub-and-spoke/main.tf
@@ -54,18 +54,25 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   use_remote_gateways       = false
 }
 
+resource "azurerm_public_ip" "nic_spoke" {
+  name                = "pip-spoke"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "nic_spoke" {
   name                = "nic-spoke"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
-
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.spoke_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.nic_spoke.id
   }
-
 }
 
 resource "azurerm_linux_virtual_machine" "vm_spoke" {
@@ -119,32 +126,34 @@ resource "azurerm_network_interface_security_group_association" "this" {
 }
 
 # Bastion
-resource "azurerm_subnet" "bastion_subnet" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.vnet_hub.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+# resource "azurerm_subnet" "bastion_subnet" {
+#   name                 = "AzureBastionSubnet"
+#   resource_group_name  = azurerm_resource_group.this.name
+#   virtual_network_name = azurerm_virtual_network.vnet_hub.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
 
-resource "azurerm_public_ip" "this" {
-  name                = "bastion-pip"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+# resource "azurerm_public_ip" "this" {
+#   name                = "bastion-pip"
+#   location            = azurerm_resource_group.this.location
+#   resource_group_name = azurerm_resource_group.this.name
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
 
-resource "azurerm_bastion_host" "this" {
-  name                = "bastion-host"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  sku                 = "Standard"
+# resource "azurerm_bastion_host" "this" {
+#   name                = "bastion-host"
+#   location            = azurerm_resource_group.this.location
+#   resource_group_name = azurerm_resource_group.this.name
+#   sku                 = "Standard"
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastion_subnet.id
-    public_ip_address_id = azurerm_public_ip.this.id
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.bastion_subnet.id
+#     public_ip_address_id = azurerm_public_ip.this.id
 
-  }
-}
+#   }
+# }
+
+#TODO: Firewall NVA integration
 
