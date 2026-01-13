032ee2007f47f52dcaa3e59f0779e7f7fdf24c1d
Diogo Guedes <diogo.f.guedes@tentwentyone.io>
2026-01-13 10:34:57 +0000

    feat: add Azure Bastion resources including subnet, public IP, and bastion host configuration

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,6 +4,36 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+# Bastion
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "this" {
+  name                = "bastion-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "this" {
+  name                = "bastion-host"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  tunneling_enabled = true # Enables Native Client Connection
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.this.id
+  }
+}
 resource "random_integer" "suffix" {
   min = 10000
   max = 99999
@@ -24,13 +54,13 @@ resource "azurerm_storage_account" "sa_dev" {
 
 }
 
-resource "azurerm_public_ip" "pip_vm" {
-  name                = "pip-vm"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+# resource "azurerm_public_ip" "pip_vm" {
+#   name                = "pip-vm"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
 resource "azurerm_subnet" "hub_subnet" {
   name                 = "hub-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -54,7 +84,7 @@ resource "azurerm_network_interface" "nic_vm" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.hub_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip_vm.id
+    # public_ip_address_id          = azurerm_public_ip.pip_vm.id
   }
 }
 
